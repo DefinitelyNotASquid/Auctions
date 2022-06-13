51c1fc77272e1fc58a5c53805ab62d759da2a6c8
Liam Carney <liammitchelcarney.au@gmail.com>
2022-06-13 22:55:02 +1000

    fix: minecraft reflection names fix: xp required to repair from nbt reflection fix: tags from nbt reflection

--- src/main/java/com/mith/Auctions/util/ReflectionUtil.java
+++ src/main/java/com/mith/Auctions/util/ReflectionUtil.java
@@ -5,6 +5,7 @@ import net.md_5.bungee.api.chat.ItemTag;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.chat.hover.content.Item;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
 import java.lang.reflect.Method;
@@ -16,26 +17,10 @@ public class ReflectionUtil {
     private static final String VERSION = Bukkit.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3] + ".";
 
 
-
-
     public static String getMinecraftName(ItemStack is) {
         try {
-            Object nmsStack = asNMSCopy(is);
-
-            //c = getItem
-            Object item = nmsStack.getClass().getMethod("c").invoke(nmsStack);
-
-            if (getItemBannerClass().isAssignableFrom(item.getClass())) {
-                Object enumColor = item.getClass().getMethod("c", nmsStack.getClass()).invoke(item, nmsStack);
-                String color = enumColor.getClass().getMethod("d").invoke(enumColor).toString();
-                return "item.banner." + color + ".name";
-            } else {
-                //m = getName
-                Object translatableComponent = item.getClass().getMethod("m", nmsStack.getClass()).invoke(item, nmsStack);
-                //i = getkey
-                String content = translatableComponent.getClass().getMethod("i").invoke(translatableComponent).toString();
-                return content;
-            }
+                Material material = is.getType();
+                return (material.isBlock() ? "block" : "item") + ".minecraft." + material.toString().toLowerCase();
         } catch (Exception ex) {
             System.out.println("Error getting minecraft name for " + is.getType() + "\n" +
                     ex);
@@ -46,14 +31,15 @@ public class ReflectionUtil {
     public static int getXPForRepair(ItemStack is) {
         try {
             Object nmsStack = asNMSCopy(is);
+
             //r = hasTag
-            boolean hasTag = (boolean) nmsStack.getClass().getMethod("r").invoke(nmsStack);
+            boolean hasTag = (boolean) nmsStack.getClass().getMethod("t").invoke(nmsStack);
 
             if (!hasTag)
                 return 0;
 
             //s = getTag
-            Object tag = nmsStack.getClass().getMethod("s").invoke(nmsStack);
+            Object tag = nmsStack.getClass().getMethod("u").invoke(nmsStack);
 
             //q = hasKey
             boolean hasKey = (boolean) tag.getClass().getMethod("q", String.class).invoke(tag, "RepairCost");
@@ -81,12 +67,12 @@ public class ReflectionUtil {
         try {
             Object nmsStack =  asNMSCopy(item);
 
-            boolean hasTag = (boolean) nmsStack.getClass().getMethod("r").invoke(nmsStack);
+            boolean hasTag = (boolean) nmsStack.getClass().getMethod("t").invoke(nmsStack);
 
             if (!hasTag)
                 return "";
 
-            Object tag = nmsStack.getClass().getMethod("s").invoke(nmsStack);
+            Object tag = nmsStack.getClass().getMethod("u").invoke(nmsStack);
 
             return tag.toString();
         } catch (Exception ex) {
@@ -118,29 +104,8 @@ public class ReflectionUtil {
         }
     }
 
-    public static Class<?> getNMSClass(String nmsClass, String nmsPackage) {
-        try {
-            return Class.forName(nmsPackage + "." + nmsClass);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
     private static Class<?> getCraftItemStackClass() {
         return getOBCClass("inventory.CraftItemStack");
-    }
-
-    private static Class<?> getLocaleLanguageClass() {
-        return getNMSClass("Language", "net.minecraft.locale");
-    }
-
-    private static Class<?> getNBTTagCompoundClass() {
-        return getNMSClass("NBTTagCompound", "net.minecraft.nbt");
-    }
-
-    private static Class<?> getItemBannerClass() {
-        return getNMSClass("ItemBanner", "net.minecraft.world.item");
     }
 
 }
